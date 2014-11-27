3da68292e8a0dd79a089be3ab19ebfd9d4936827
whalebot_helmsman <whalebot.helmsman@gmail.com>
2014-11-27 17:55:39 +0300

    remove old junk

--- link/link_buffer.cpp
+++ link/link_buffer.cpp
@@ -3,52 +3,62 @@
 #include "link_buffer.h"
 
 
-void CLinkBuffer::writeUsedLinks(std::ostream &uo)const{
-    for(boost::unordered_set<CLink>::const_iterator i = m_usedLinks.begin();
-        i != m_usedLinks.end(); ++i){
-        uo<<*i;
+void CLinkBuffer::writeUsedLinks(std::ostream &uo)const
+{
+    for ( boost::unordered_set<CLink>::const_iterator i = m_usedLinks.begin()
+        ; i != m_usedLinks.end()
+        ; ++i ) {
+        uo << *i;
     }
 }
 
-void CLinkBuffer::writeFutureLinks(std::ostream &fo)const{
-    for(std::list<CLink>::const_iterator i = m_futureLinks.begin();
-        i != m_futureLinks.end(); ++i){
-        fo<<*i;
+void CLinkBuffer::writeFutureLinks(std::ostream &fo)const
+{
+    for ( std::list<CLink>::const_iterator i = m_futureLinks.begin()
+        ; i != m_futureLinks.end()
+        ; ++i ) {
+        fo << *i;
     }
 }
-void CLinkBuffer::readFutureLinks(std::istream &ui){
+
+void CLinkBuffer::readFutureLinks(std::istream &ui)
+{
     CLink    buff;
-    while(ui>>buff){
+    while(ui >> buff) {
         m_futureLinks.push_back(buff);
     }
 }
-void CLinkBuffer::readUsedLinks(std::istream &fi){
+
+void CLinkBuffer::readUsedLinks(std::istream &fi)
+{
     CLink    buff;
-    while(fi>>buff){
+    while(fi >> buff) {
         m_usedLinks.insert(buff);
     }
 
 }
 
-bool CLinkBuffer::pop(CLink &top){
-    bool    ret(false);
-    if(!m_futureLinks.empty()){
-        ret =   true;
-        top =   m_futureLinks.front();
-        m_futureLinks.pop_front();
+bool CLinkBuffer::pop(CLink &top)
+{
+    if(true == m_futureLinks.empty()) {
+        return false;
     }
-    return ret;
+
+    top =   m_futureLinks.front();
+    m_futureLinks.pop_front();
+
+    return true;
 }
 
-size_t CLinkBuffer::size()const{
+size_t CLinkBuffer::size()const
+{
     return m_futureLinks.size();
 }
 
-
-bool CLinkBuffer::empty()const{
+bool CLinkBuffer::empty()const
+{
     return m_futureLinks.empty();
 }
-
 
 bool CLinkBuffer::IsFutureEmpty()const
 {
@@ -60,8 +70,9 @@ bool CLinkBuffer::IsUsedEmpty()const
     return m_usedLinks.empty();
 }
 
-void CLinkBuffer::pushLink(CLink const &link){
-    if(m_usedLinks.find(link) == m_usedLinks.end()){
+void CLinkBuffer::pushLink(CLink const &link)
+{
+    if (m_usedLinks.find(link) == m_usedLinks.end()) {
         m_usedLinks.insert(link);
         m_futureLinks.push_back(link);
     }
